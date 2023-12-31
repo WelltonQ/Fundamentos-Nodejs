cdcb16adf6100cf5d6581fa51df766eb7b548ec1
Wellton Quirino <welltonq@outlook.com>
2023-12-31 11:53:44 -0300

    application structure

--- src/server.js
+++ src/server.js
@@ -1,17 +1,31 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
 	const { method, url } = req
 
 	if (method === "GET" && url === "/users") {
-		return res.end("Listar usuários")
+		return res
+			.setHeader("Content-type", "application/json")
+			.end(JSON.stringify(users))
 	}
 
 	if (method === "POST" && url === "/users") {
-		return res.end("Criar usuário")
+		users.push({
+			id: 1,
+			name: "Wellton",
+			email: "welltonq@outlook.com",
+		})
+
+		return res
+			.writeHead(201)
+			.end("Created user with success!")
 	}
 
-	return res.end("Test")
+	return res
+		.writeHead(404)
+		.end("Nout found")
 })
 
 server.listen(3333)
