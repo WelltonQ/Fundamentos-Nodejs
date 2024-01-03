ae8e98e2fb23bf82a83bfdd9be114fa0f148064a
Wellton Quirino <welltonq@outlook.com>
2024-01-03 09:43:17 -0300

    add streams, middleware and fake database

--- src/server.js
+++ src/server.js
@@ -1,22 +1,32 @@
 import http from 'node:http'
+import { randomUUID } from "node:crypto"
 
-const users = []
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const server = http.createServer((req, res) => {
+const database = new Database()
+
+const server = http.createServer(async (req, res) => {
 	const { method, url } = req
 
+	await json(req, res)
+
 	if (method === "GET" && url === "/users") {
-		return res
-			.setHeader("Content-type", "application/json")
-			.end(JSON.stringify(users))
+		const users = database.select("users")
+
+		return res.end(JSON.stringify(users))
 	}
 
 	if (method === "POST" && url === "/users") {
-		users.push({
-			id: 1,
-			name: "Wellton",
-			email: "welltonq@outlook.com",
-		})
+		const { name, email } = req.body
+
+		const user = {
+			id: randomUUID(),
+			name: name,
+			email: email,
+		}
+
+		database.insert("users", user)
 
 		return res
 			.writeHead(201)
